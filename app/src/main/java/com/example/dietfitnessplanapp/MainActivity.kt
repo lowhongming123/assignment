bfd4449ca0f3f8a7cf41ebc3e9e1c9cfd9fb1295
ming <lowhm-wm17@student.tarc.edu.my>
2019-12-24 13:11:53 +0800

    Initial commit

--- app/src/main/java/com/example/dietfitnessplanapp/MainActivity.kt
+++ app/src/main/java/com/example/dietfitnessplanapp/MainActivity.kt
@@ -39,50 +39,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun login(){
+
+    /*
         val intent= Intent(Intent.ACTION_VIEW)
         val phone:String = "tel :0123456789"
 
-        //Check package manager for app to handle an intent
-        intent.setData(Uri.parse(phone))
+        Check package manager for app to handle an intent
+       intent.setData(Uri.parse(phone))
         if(intent.resolveActivity(packageManager) !== null){
-            startActivity(intent)
-        }
 
+        }
+        */
+
+        val intent = Intent(this,WeightActivity::class.java)
+        startActivity(intent)
 
     }
 
-
+ /*
     private fun register(){
 
 
-        //Explicit
+        Explicit
         val intent = Intent(this,SecondActivity::class.java)
 
 
-        //val username= editTextUsername.text.toString()
-        //val password=editTextPassword.text.toString()
-        //intent.putExtra(KEY,username)
-        //intent.putExtra(KEY2,password)
+        val username= editTextUsername.text.toString()
+        val password=editTextPassword.text.toString()
+        intent.putExtra(KEY,username)
+        intent.putExtra(KEY2,password)
 
          startActivity(intent)  // An intent without return value
-        //startActivityForResult(intent,REQUEST_CODE)
+        startActivityForResult(intent,REQUEST_CODE)
 
     }
 
-    //override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
-        //if(requestCode== REQUEST_CODE){
-            //if(resultCode== Activity.RESULT_OK){
-                //val reply=data?.getStringExtra(KEY3).toString()
-               // textViewReply.text=String.format(" %s  %s ",getString(R.string.reply),reply)
-            //}
-        //}
-    //}
+        if(requestCode== REQUEST_CODE){
+            if(resultCode== Activity.RESULT_OK){
+                val reply=data?.getStringExtra(KEY3).toString()
+                textViewReply.text=String.format(" %s  %s ",getString(R.string.reply),reply)
+            }
+        }
+    }
 
-
-
-
+*/
 
 
 
-}
+
---
+++ app/src/main/java/com/example/dietfitnessplanapp/WeightActivity.kt
@@ -0,0 +1,87 @@
+package com.example.dietfitnessplanapp
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_weight.*
+import android.widget.TextView
+import android.widget.Toast
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import android.R
+import com.jjoe64.graphview.GraphView
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
+
+
+class WeightActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weight)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+        val dp = arrayOf<DataPoint>(DataPoint(0, 0), DataPoint(1, 1), DataPoint(2, 2))
+        val barGraphSeries = BarGraphSeries(dp)
+        val graph = findViewById(R.id.graph) as GraphView
+        graph.visibility = View.VISIBLE
+        barGraphSeries.resetData(dp);
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_help -> {
+                Toast.makeText(this, "Help clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_weight -> {
+                Toast.makeText(this, "Weight clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_diary -> {
+                Toast.makeText(this, "Diary clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_updateProfile -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Sign out clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
